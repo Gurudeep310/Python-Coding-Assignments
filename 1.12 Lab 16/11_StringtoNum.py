b8b2a3678bdf5dd1886088351966bee7d2efd7d8
Gurudeep <gurudheep2002@gmail.com>
2022-10-02 20:39:10 +0530

    Convert String to number

---
+++ 1.12 Lab 16/11_StringtoNum.py
@@ -0,0 +1,15 @@
+'''
+[L16] Convert String to number
+Given a string S, write a program to convert the given string into an integer and print the integer.
+Input:
+    123
+    where:
+First line represents the input string
+Output:
+    123
+Assumptions:
+    Length of the string S can be 0 to 10
+'''
+number = input(" ")
+number = int(number)
+print(number)
