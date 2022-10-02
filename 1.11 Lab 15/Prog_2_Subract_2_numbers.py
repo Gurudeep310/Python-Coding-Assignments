330d915d16e1263b5277ac06ecb57ef21f72a044
Gurudeep <gurudheep2002@gmail.com>
2022-10-02 20:38:11 +0530

    Subtraction of a number from another

---
+++ 1.11 Lab 15/Prog_2_Subract_2_numbers.py
@@ -0,0 +1,17 @@
+'''
+[L15 P2] Subtraction of a number from another 
+Tags:
+L15
+Given two integers N1 and N2, subtract the latter from the former and store the result in R.
+The code to read N1 and N2 as well as to print R has already been provided.
+Input
+    20
+    10
+Output
+
+    10
+'''
+N1 = int(input())
+N2 = int(input())
+R = N1 - N2
+print(R)
