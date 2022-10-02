fb01029d1c4830068d598e4bdedbb6b82b9576b8
Gurudeep <gurudheep2002@gmail.com>
2022-10-02 20:38:04 +0530

    Sum of two numbers

---
+++ 1.11 Lab 15/Prog_1_Sum_of_2_numbers.py
@@ -0,0 +1,20 @@
+'''
+[L15 P1] Sum of two numbers
+Given two integers N1 and N2, find their sum and store in R.
+The code to read N1 and N2 as well as to print R has already been provided.
+Input
+    10
+    20
+Output
+
+    30
+'''
+N1 = int(input())
+N2 = int(input())
+
+R = N1 + N2
+
+
+
+
+print(R)
