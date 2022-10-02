ae6531a7cafcf5715ee5d9e444d7443788241480
Gurudeep <gurudheep2002@gmail.com>
2022-10-02 20:38:17 +0530

    Division of a number by another

---
+++ 1.11 Lab 15/Prog_3_Division_of_number.py
@@ -0,0 +1,14 @@
+'''
+[L15 P3] Division of a number by another
+Given two integers N1 and N2, divide the former by the latter and store the result in R.
+The code to read N1 and N2 as well as to print R has already been provided.
+Input
+    50
+    5
+Output
+    10.00
+'''
+N1 = float(input())
+N2 = float(input())
+R = N1/N2
+print("%.2f" % R)
